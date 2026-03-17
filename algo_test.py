b15345e923d019cebde9d4e3b030c449e8eb855c
ian6234 <ian.averre@yahoo.com>
2026-03-17 13:22:15 +0000

    added threshold to market-taking execution logic

--- algo_test.py
+++ algo_test.py
@@ -1,3 +1,8 @@
+import math
+
+import jsonpickle
+import numpy as np
+
 from datamodel import OrderDepth, UserId, TradingState, Order
 from typing import List
 import string
@@ -31,11 +36,12 @@ class Trader:
         return 10000
 
     def tomato_price(self, state):
+
         return self.volume_weighted_mid("TOMATOES", state)
 
 
     # Execution Logic
-    def basic_execution(self, product, pos_limit, position, fair_value, state: TradingState):
+    def basic_execution(self, product, pos_limit, position, fair_value, threshold, state: TradingState):
 
         order_depth = state.order_depths[product]
         orders = []
@@ -55,7 +61,7 @@ class Trader:
             max_quantity = pos_limit - position
             for ask, quantity in list(order_depth.sell_orders.items()):
                 # if ask below fair value, buy
-                if float(ask) - fair_value < 0:
+                if float(ask) + threshold - fair_value < 0:
                     # if current quantity exceeds position limit, stop.
                     capacity = max_quantity - abs(buy_quantity)
                     if capacity <= 0:
@@ -84,7 +90,7 @@ class Trader:
             max_quantity = pos_limit + position
             for bid, quantity in list(order_depth.buy_orders.items()):
                 # if bid above fair value, sell
-                if float(bid) - fair_value > 0:
+                if float(bid) - threshold - fair_value > 0:
                     # if current quantity exceeds position limit, stop.
                     capacity = max_quantity - abs(sell_quantity)
                     if capacity <= 0:
@@ -107,6 +113,10 @@ class Trader:
                 orders.append(Order(product, last_bid, -sell_quantity))
                 new_position = position - sell_quantity
 
+        return orders, new_position, remaining_bids, remaining_asks
+
+    def market_make(self, product, pos_limit, new_position, fair_value, remaining_bids, remaining_asks, edge):
+        orders = []
         # Maker Logic
 
         if len(remaining_asks) != 0:
@@ -114,19 +124,24 @@ class Trader:
             last_ask, amount = list(remaining_asks.items())[0]
             if last_ask - 1 > fair_value:
                 ask_quantity = pos_limit + new_position
-            #    print(f"ASK {product}", str(-ask_quantity) + "x", last_ask - 1)
+                #    print(f"ASK {product}", str(-ask_quantity) + "x", last_ask - 1)
                 orders.append(Order(product, last_ask - 1, -ask_quantity))
+
+        else:
+            ask_quantity = pos_limit + new_position
+            orders.append(Order(product, fair_value + edge, -ask_quantity))
 
         if len(remaining_bids) != 0:
 
             last_bid, amount = list(remaining_bids.items())[0]
             if last_bid + 1 < fair_value:
                 bid_quantity = pos_limit - new_position
-             #   print(f"BID {product}", str(bid_quantity) + "x", last_bid + 1)
+                #   print(f"BID {product}", str(bid_quantity) + "x", last_bid + 1)
                 orders.append(Order(product, last_bid + 1, bid_quantity))
-
-        return orders, new_position
-
+        else:
+            bid_quantity = pos_limit - new_position
+            orders.append(Order(product, fair_value - edge, bid_quantity))
+        return orders
 
     def bid(self):
         return 15
@@ -140,6 +155,16 @@ class Trader:
        # print("traderData: " + state.traderData)
       #  print("Observations: " + str(state.observations))
 
+
+        if state.timestamp == 0:
+
+            trader_data = {
+                "EMERALDS": {},
+                "TOMATOES": {
+                },
+            }
+        else:
+            trader_data = jsonpickle.decode(state.traderData)
         # Orders to be placed on exchange matching engine
         result = {}
         for product in state.order_depths:
@@ -148,24 +173,33 @@ class Trader:
 
             current_position = state.position.get(product, 0)
 
+            edge = 1
+
             fair_value = 10
             if product == "EMERALDS":
                 fair_value = self.emerald_price()  # Participant should calculate this value
+                edge = 7
+                threshold = 0
             elif product == "TOMATOES":
                 fair_value = self.tomato_price(state)
+                fair_value = round(fair_value)
+                edge = 6
+                threshold = 2
 
-            if fair_value == -1:
-                continue
            # print("Acceptable price : " + str(fair_value))
            # print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", Sell order depth : " + str(
            #     len(order_depth.sell_orders)))
 
             # Order Execution logic
             # Basic logic: clear the market, then place resting orders.
-            orders, new_position = self.basic_execution(product, pos_limits[product], current_position, fair_value, state)
+            instant_orders, new_position, remaining_bids, remaining_asks = self.basic_execution(product, pos_limits[product], current_position, fair_value, threshold, state)
+            resting_orders = self.market_make(product, pos_limits[product], new_position, fair_value, remaining_bids, remaining_asks, edge)
 
-            result[product] = orders
 
-        traderData = "test!"  # No state needed - we check position directly
+            result[product] = instant_orders
+            result[product].extend(resting_orders)
+
+        trader_data_encode = jsonpickle.encode(trader_data)
+
         conversions = 0
-        return result, conversions, traderData
+        return result, conversions, trader_data_encode
